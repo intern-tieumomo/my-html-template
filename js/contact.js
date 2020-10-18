b2367d769d5a47bd901004ec5d5794f4f3143c5b
intern-tieumomo <intern.tieumomo@gmail.com>
2020-10-19 01:15:53 +0700

    Update Contact Form
    Update Contact Form

--- js/contact.js
+++ js/contact.js
@@ -8,9 +8,27 @@ $(document).ready(function(e) {
         var subject = $('#subject').val();
         var message = $('#message').val();
 
-        var $form = $(this);
-        $.post($form.attr("action"), $form.serialize()).then(function() {
-            alert("Thank you!");
-        });
+        if (fname == "" || lname == "") {
+            swal('Pylate', 'Name is required!', 'warning');
+        } else if (email == ""){
+            swal('Pylate', 'Email is required!', 'warning');
+        } else if (subject == ""){
+            swal('Pylate', 'Subject is required!', 'warning');
+        } else if (message == ""){
+            swal('Pylate', 'Message is required!', 'warning');
+        } else {
+            $.ajax({
+                url: "",
+                type: "POST",
+                data: $(this).serialize(),
+                beforeSend: function() {
+                    $('#submit').val('Sending ...');
+                },
+                success: function() {
+                    $('#submit').val('Send Message');
+                    swal('Pylate', 'We received your contact!', 'success');
+                }
+            });
+        }
     });
 });
